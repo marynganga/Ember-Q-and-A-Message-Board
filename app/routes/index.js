271ea0de092fceda77c2d6ef0a8156b37a337a55
marynganga <marynganga.mw@gmail.com>
2017-09-22 15:42:25 +0300

    Connect the saving a new Question component to the index route to be saved in the database.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model(){
 		return Ember.RSVP.hash({
-			questions: this.store.findAll('question'),
-			answers: this.store.findAll('answer')
+			questions: this.get('store').findAll('question'),
+			answers: this.get('store').findAll('answer'),
+		
 		})
+	},
+	actions: {
+		saveQuestion(params){
+			var newQuestion = this.store.createRecord('question', params);
+			newQuestion.save();
+			this.transitionTo('index');
+		}
 	}
 });

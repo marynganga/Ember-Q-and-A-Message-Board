61afc456b8b20e08325781b1028c67e571ee5e81
marynganga <marynganga.mw@gmail.com>
2017-09-22 13:15:06 +0300

    Generate a route for the About Page. 1. Input content.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -22,7 +22,7 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
-  app.import('bower_components/bootstrap/distjs/bootstrap.js')
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js')
 
   return app.toTree();
 };

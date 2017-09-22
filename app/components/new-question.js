727333d28b0f1dd37f8e55a3f464ba5cc0bb8033
marynganga <marynganga.mw@gmail.com>
2017-09-22 15:05:55 +0300

    Add functionality to the new-question component. 1. Show the form when add question button is clicked. 2. Hide the form and clear the input fields when the close button is clicked.

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	newQuestionForm: false,
+	actions:{
+		addQuestionClicked(){
+			this.set('newQuestionForm', true);
+		},
+		closeQuestionForm(){
+			this.set('newQuestionForm', false);
+			this.set('author', "");
+			this.set('content', "");
+			this.set('notes', "");
+		}
+	}
 });

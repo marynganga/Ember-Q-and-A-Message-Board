fb3c963040c672dbfa88db689d6318a2f4b2f678
marynganga <marynganga.mw@gmail.com>
2017-09-22 15:12:53 +0300

    Add saving the input functionality to the new question component.

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -11,6 +11,16 @@ export default Ember.Component.extend({
 			this.set('author', "");
 			this.set('content', "");
 			this.set('notes', "");
+		},
+		saveQuestion(){
+			var params = {
+				author: this.get('author')?this.get('author'):"",
+				content: this.get('content')?this.get('content'):"",
+				notes: this.get('notes')?this.get('notes'):"",
+			};
+			this.set("newQuestionForm",false);
+			this.sendAction('saveQuestion', params);
+			console.log(params)
 		}
 	}
 });
